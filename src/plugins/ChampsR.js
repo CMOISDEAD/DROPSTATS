d72d3bf135fa6741d3eba21e1e76f3718acb4553
CMOISDEAD <camilodavina@gmail.com>
2020-06-01 17:44:37 -0500

    Dandole estilo a la pantalla principal

--- src/plugins/ChampsR.js
+++ src/plugins/ChampsR.js
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
 import "../App.css";
 
+import newChamp from "../assets/Volibear.jpg"
+
 import { data } from "../champlist.json";
 import { ritokey } from "../KEY/RIOTAPIKEY.json";
 
@@ -40,18 +42,29 @@ class champsR extends Component {
     var waiting = this.state.freeChampionIds.map((freeChampionIds, i) => {
         var index = this.state.freeChampionIds[i]
       return (
-        <img
-          src={`http://ddragon.leagueoflegends.com/cdn/10.11.1/img/champion/${data[index].ChampName}.png`}
-          alt={freeChampionIds}
-          className="p-3"
-          key={i}
-        />
+        <a href={"champions/" + data[index].ChampName}>
+          <img
+            src={`http://ddragon.leagueoflegends.com/cdn/10.11.1/img/champion/${data[index].ChampName}.png`}
+            alt={freeChampionIds}
+            className="champs-rotation vanish"
+            key={i}
+          />
+        </a>
       );
     });
 
     return (
       <div className="row">
-        <div className="col m-0">
+        <div className="col ">
+          <div className="paragraf-box new-champ-art">
+            <div className="story-box">
+              <p className="champ-title">Volibear</p>
+              <p className="champ-slogan">The relentless of storm</p>
+            </div>
+            <img src={newChamp} alt=""/>
+          </div>
+        </div>
+        <div className="col ">
           <h3 className="text-center main-color">Champion Rotation</h3>
           {waiting}
         </div>
